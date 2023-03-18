428e5f5c94e2202f2c98a99955f45ccfa39fd4e7
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-03-18 08:39:29 +0000

    3/18

---
+++ db/migrate/20230317061616_create_post_images.rb
@@ -0,0 +1,12 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+
+      t.timestamps
+      #t.integer :id
+      t.integer :user_id
+      t.string :shop_name
+      t.text :caption
+    end
+  end
+end
